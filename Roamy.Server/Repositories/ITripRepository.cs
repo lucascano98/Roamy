9981edb18ef84297c18d40396fedc212a0da9515
lucas <lucas@LUCASCANO-TP>
2026-04-07 22:32:26 +0530

    Add IRepository folder and create ITripRepository

---
+++ Roamy.Server/Repositories/ITripRepository.cs
@@ -0,0 +1,13 @@
+﻿using Roamy.Shared.Models;
+
+namespace Roamy.Server.Repositories
+{
+    public interface ITripRepository
+    {
+        Task<Trip> GetTripByIdAsync(Guid id);
+        Task<IEnumerable<Trip>> GetAllTripsAsync();
+        Task<Trip> AddTripAsync(Trip trip);
+        Task UpdateTripAsync(Trip trip);
+        Task DeleteTripAsync(Guid id);
+    }
+}
