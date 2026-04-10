137109fe822cf09a9b8638ffa2c81cca0b17c716
lucas <lucas@LUCASCANO-TP>
2026-04-10 06:05:47 +0530

    Change HttpClient BaseAddress

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//Replace with environment variable before deploying to Azure
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7104") });
 builder.Services.AddSingleton<TripManager>(); //instantiates one TripManager object for the duration of the program. Different from Transient and Scope.
 
 await builder.Build().RunAsync();
