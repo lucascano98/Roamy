ddb9e90ff5afb6c1d6619cf9c7e585514759e672
lucas <lucas@LUCASCANO-TP>
2026-04-15 20:03:46 +0530

    fix: update BaseAddress to HTTPS, fix Latitude typo in TripLocation, add FixLatitudeTypo migration, add Mapbox token URL restriction for Windows dev environment

---
+++ Roamy.Server/Migrations/20260415140939_FixLatitudeTypo.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Roamy.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixLatitudeTypo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Latitue",
+                table: "TripLocations",
+                newName: "Latitude");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Latitude",
+                table: "TripLocations",
+                newName: "Latitue");
+        }
+    }
+}
