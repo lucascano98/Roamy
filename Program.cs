5333029796640d8ec6bd82dfff8e093b80204b59
lucascano98 <lucascano98@outlook.com>
2026-04-28 01:10:27 +0530

    Update localhost port

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 //Replace with environment variable before deploying to Azure
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7104") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5291") });
 builder.Services.AddScoped<TripManager>(); //instantiates one TripManager object for the duration of the program. Different from Transient and Scope.
 
 await builder.Build().RunAsync();
